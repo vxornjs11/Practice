02522681dfd29ee86ce69bd691e12080e9ef40b9
박태권 <ptk@bagtaegwon-ui-MacBookAir.local>
2024-08-14 16:40:24 +0900

    options매일로 된건 처리가 된듯

--- lib/screen/Calendar.dart
+++ lib/screen/Calendar.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get_utils/get_utils.dart';
 
 class calendar extends StatefulWidget {
   const calendar({
@@ -44,55 +46,96 @@ class _calendarState extends State<calendar> {
     Map<DateTime, int> Clear_dateCounts = {};
     // 각 달의 문서 개수를 계산
     for (var doc in documents) {
-      DateTime YMD_now;
-      YMD_now = DateTime(doc['year'], doc['month'], doc['day']);
+      int currentYear = DateTime.now().year;
+      int currentMonth = DateTime.now().month;
+
+      // 다음 달의 첫 번째 날에서 하루를 빼면 현재 월의 마지막 날이 나옵니다.
+      DateTime firstDayOfNextMonth = (currentMonth < 12)
+          ? DateTime(currentYear, currentMonth + 1, 1)
+          : DateTime(currentYear + 1, 1, 1); // 다음 해의 1월인 경우 처리
+
+      DateTime lastDayOfCurrentMonth =
+          firstDayOfNextMonth.subtract(Duration(days: 1));
+
+      int daysInMonth = lastDayOfCurrentMonth.day;
+
+      // print('현재 월의 마지막 날: $lastDayOfCurrentMonth');
+      // print('이번 달의 일 수: $daysInMonth');
+      DateTime YMD_now = DateTime(doc['year'], doc['month'], doc['day']);
       String options = doc['option'];
       int month = doc['month'];
       // 날짜 차이 계산
       Duration difference = DateTime.now().difference(YMD_now);
       bool hasMonthChanged = DateTime.now().year != YMD_now.year ||
           DateTime.now().month != YMD_now.month;
-
-      // 차이나는 일(day) 수
       int differenceInDays = difference.inDays;
-      print('options: $options');
-      print('현재 날짜: ${DateTime.now()}');
-      print('비교 날짜: $YMD_now');
-      print('차이나는 일 수: $differenceInDays');
-      print('월이 바뀌었나? $hasMonthChanged');
-      print('지금 무슨 요일이지? ${YMD_now.weekday} ');
-      if (monthCounts.containsKey(month) && options == "매일") {
-        bool hasMonthChanged = DateTime.now().year != YMD_now.year ||
-            DateTime.now().month != YMD_now.month;
-// 7월은 18일이 나와야 됨. 지금 너무 높다.
+      // print('options: $options');
+      // print('현재 날짜: ${DateTime.now()}');
+      // print('비교 날짜: $YMD_now');
+      // print('차이나는 일 수: $differenceInDays');
+      // print('월이 바뀌었나? $hasMonthChanged');
+      // print('지금 무슨 요일이지? ${YMD_now.weekday} ');
+      // 7월은 20일이 나와야 됨. 지금 너무 높다.
 // 8월은 오늘 4개 추가했고 7월에 시작하는 매일 일정이 4개니까
-// 13곱하기 4 로 52에 +4로 56개여야함.
+//     ++++ 14곱하기 4 로 56에 +4로 60개여야함.
+//    ++++ 14일 기준으로 어 쉬발 뭐지 된건가.
+//    ++++ 주말 주중 1년 1달을 +1로만 처리해서 정확하진 않음.
+//    ++++ 근데 지금 매일만 처리하는 기준으로는 된듯.
 // 지금 true면 차이나는 일수만큼 그냥 더해버리는데
 // 근데 다음달은 초기화 해서 1이 되버리고 7월에만 계속 추가됨. 골떄리는데?
 // 달이 바뀌었나 이거는 써도 되는데 차이나는 일수 이게 그냥 joat네 쓰면안됨.
-        if (hasMonthChanged) {
-          monthCounts[month] = (monthCounts[month]! + differenceInDays);
-          // 현재 달에 데이터 추가
-          print('ture일경우 ${monthCounts[month]} ');
-          // 다음 달 계산 (현재 달이 12월이면 다음 달은 1월이 됩니다)
-          int nextMonth = (month % 12) + 1;
-
-          // 다음 달에 데이터 추가
-          print('nextMonth ${nextMonth} ');
-          monthCounts[nextMonth] =
-              (monthCounts[nextMonth] ?? 0) + differenceInDays;
-          print('nextMonth2 ${monthCounts[nextMonth]} ');
-        } else {
-          monthCounts[month] = (monthCounts[month]! + differenceInDays);
-          print('flase일경우 ${monthCounts[month]} ');
-        }
-      } else {
-        monthCounts[month] = 1;
-      }
-      print(monthCounts);
+// 달이 바뀌었나? 이거 true면 그냥 now에서 month빼면 day만 남을거아님 그거 넣으면 되겟다.
+// 그럼 안바뀌었으면 해당 달에서 - day하면되네.
       print("===================");
       print("options$options");
       print("===================");
+
+      if (options != null && monthCounts != null && YMD_now != null) {
+        bool hasMonthChanged = DateTime.now().year != YMD_now.year ||
+            DateTime.now().month != YMD_now.month;
+        print("hasMonthChanged$hasMonthChanged");
+        if (options == "매일") {
+          if (hasMonthChanged) {
+            // 현재 달에 데이터 추가
+            print('달이 바뀐 경우 (true): $hasMonthChanged');
+            print('현재 달의 일정 개수: ${monthCounts[month]}');
+
+            // 다음 달 계산 (현재 달이 12월이면 다음 달은 1월이 됩니다)
+            int nextMonth = (month % 12) + 1;
+            int daysInMonth =
+                DateTime(YMD_now.year, YMD_now.month + 1, 0).day; // 현재 월의 일 수
+            int differenceDays = daysInMonth - YMD_now.day + 1; // 남은 일 수 계산
+
+            print("7월 매일 일정: $differenceDays");
+            monthCounts[month] = (monthCounts[month] ?? 0) + differenceDays;
+
+            // 다음 달에 데이터 추가
+            print('다음 달 (nextMonth): $nextMonth');
+            monthCounts[nextMonth] =
+                (monthCounts[nextMonth] ?? 0) + DateTime.now().day;
+            print('다음 달의 일정 개수: ${monthCounts[nextMonth]}');
+          } else {
+            int differenceDays = DateTime.now().day;
+            print("8월 매일 일정: $differenceDays");
+
+            // 현재 달에 데이터 추가
+            monthCounts[month] = (monthCounts[month] ?? 0) + differenceDays;
+            print('달이 바뀌지 않은 경우 (false): ${monthCounts[month]}');
+          }
+        } else if (options == "주말" ||
+            options == "1년" ||
+            options == "한달" ||
+            options == "주중") {
+          monthCounts[month] = (monthCounts[month] ?? 0) + 1;
+        } else {
+          monthCounts[month] = 1;
+        }
+
+        print(monthCounts);
+      } else {
+        print("options, monthCounts 또는 YMD_now가 null입니다.");
+      }
+      print(monthCounts);
     }
     List<FlSpot> spots = [];
     for (int i = 1; i <= 12; i++) {
