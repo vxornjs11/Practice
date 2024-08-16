7b65a65242a9bfa79f59e18b3c30d29a1246e74d
박태권 <ptk@bagtaegwon-ui-MacBookAir.local>
2024-08-16 16:26:22 +0900

    라인차트 끝 달성률 수정하고 마무리 단계

--- lib/screen/Calendar.dart
+++ lib/screen/Calendar.dart
@@ -37,46 +37,18 @@ class _calendarState extends State<calendar> {
     for (int i = 1; i <= 12; i++) {
       spots2.add(FlSpot(i.toDouble(), (Clear_dateCounts[i] ?? 0).toDouble()));
     }
-
+    List<double> scheduleCounts2 = spots2.map((spot) => spot.y).toList();
+    double totalSchedules2 = scheduleCounts2.reduce((a, b) => a + b);
+    print("totalSchedules2"); // 37.0
+    print(totalSchedules2); // 37.0
+    print("totalSchedules2"); // 37.0
     return spots2;
   }
 
   List<FlSpot> _generateFlSpots(List<QueryDocumentSnapshot> documents) {
     Map<int, int> monthCounts = {};
-    Map<DateTime, int> Clear_dateCounts = {};
     // 각 달의 문서 개수를 계산
-    for (var doc in documents) {
-      int currentYear = DateTime.now().year;
-      int currentMonth = DateTime.now().month;
-
-      // 다음 달의 첫 번째 날에서 하루를 빼면 현재 월의 마지막 날이 나옵니다.
-      DateTime firstDayOfNextMonth = (currentMonth < 12)
-          ? DateTime(currentYear, currentMonth + 1, 1)
-          : DateTime(currentYear + 1, 1, 1); // 다음 해의 1월인 경우 처리
-
-      DateTime lastDayOfCurrentMonth =
-          firstDayOfNextMonth.subtract(Duration(days: 1));
-
-      int daysInMonth = lastDayOfCurrentMonth.day;
-
-      // print('현재 월의 마지막 날: $lastDayOfCurrentMonth');
-      // print('이번 달의 일 수: $daysInMonth');
-      DateTime YMD_now = DateTime(doc['year'], doc['month'], doc['day']);
-      String options = doc['option'];
-      int month = doc['month'];
-      // 날짜 차이 계산
-      Duration difference = DateTime.now().difference(YMD_now);
-      bool hasMonthChanged = DateTime.now().year != YMD_now.year ||
-          DateTime.now().month != YMD_now.month;
-      int differenceInDays = difference.inDays;
-      // print('options: $options');
-      // print('현재 날짜: ${DateTime.now()}');
-      // print('비교 날짜: $YMD_now');
-      // print('차이나는 일 수: $differenceInDays');
-      // print('월이 바뀌었나? $hasMonthChanged');
-      // print('지금 무슨 요일이지? ${YMD_now.weekday} ');
-      // 7월은 20일이 나와야 됨. 지금 너무 높다.
-// 8월은 오늘 4개 추가했고 7월에 시작하는 매일 일정이 4개니까
+    // 8월은 오늘 4개 추가했고 7월에 시작하는 매일 일정이 4개니까
 //     ++++ 14곱하기 4 로 56에 +4로 60개여야함.
 //    ++++ 14일 기준으로 어 쉬발 뭐지 된건가.
 //    ++++ 주말 주중 1년 1달을 +1로만 처리해서 정확하진 않음.
@@ -86,17 +58,23 @@ class _calendarState extends State<calendar> {
 // 달이 바뀌었나 이거는 써도 되는데 차이나는 일수 이게 그냥 joat네 쓰면안됨.
 // 달이 바뀌었나? 이거 true면 그냥 now에서 month빼면 day만 남을거아님 그거 넣으면 되겟다.
 // 그럼 안바뀌었으면 해당 달에서 - day하면되네.
+    for (var doc in documents) {
+      // 다음 달의 첫 번째 날에서 하루를 빼면 현재 월의 마지막 날이 나옵니다.
+      DateTime YMD_now = DateTime(doc['year'], doc['month'], doc['day']);
+      String options = doc['option'];
+      int month = doc['month'];
+
       print("===================");
-      print("options$options");
+      print("options: $options");
       print("===================");
 
       if (options != null && monthCounts != null && YMD_now != null) {
         bool hasMonthChanged = DateTime.now().year != YMD_now.year ||
             DateTime.now().month != YMD_now.month;
-        print("hasMonthChanged$hasMonthChanged");
+        print("hasMonthChanged: $hasMonthChanged");
+
         if (options == "매일") {
           if (hasMonthChanged) {
-            // 현재 달에 데이터 추가
             print('달이 바뀐 경우 (true): $hasMonthChanged');
             print('현재 달의 일정 개수: ${monthCounts[month]}');
 
@@ -106,7 +84,7 @@ class _calendarState extends State<calendar> {
                 DateTime(YMD_now.year, YMD_now.month + 1, 0).day; // 현재 월의 일 수
             int differenceDays = daysInMonth - YMD_now.day + 1; // 남은 일 수 계산
 
-            print("7월 매일 일정: $differenceDays");
+            print("현재 달 매일 일정: $differenceDays");
             monthCounts[month] = (monthCounts[month] ?? 0) + differenceDays;
 
             // 다음 달에 데이터 추가
@@ -116,34 +94,62 @@ class _calendarState extends State<calendar> {
             print('다음 달의 일정 개수: ${monthCounts[nextMonth]}');
           } else {
             int differenceDays = DateTime.now().day;
-            print("8월 매일 일정: $differenceDays");
+            print("현재 달 매일 일정: $differenceDays");
 
-            // 현재 달에 데이터 추가
             monthCounts[month] = (monthCounts[month] ?? 0) + differenceDays;
             print('달이 바뀌지 않은 경우 (false): ${monthCounts[month]}');
           }
-        } else if (options == "주말" ||
-            options == "1년" ||
-            options == "한달" ||
-            options == "주중") {
+        } else if (options == "주중") {
+          print("YMD_now.weekday ${YMD_now.weekday}");
+          print("DateTime.monday ${DateTime.monday}");
+          print("YMD_now.weekday ${YMD_now.weekday}");
+          print("DateTime.friday ${DateTime.friday}");
+          int weeksElapsed =
+              (DateTime.now().difference(YMD_now).inDays / 7).floor();
+          int daysToAdd = 5 * (weeksElapsed + 1); // 매주 5일씩 추가
+
+          if (YMD_now.weekday >= DateTime.monday &&
+              YMD_now.weekday <= DateTime.friday) {
+            monthCounts[month] = (monthCounts[month] ?? 0) + daysToAdd;
+            print("주중 일정 추가: $daysToAdd 일");
+          }
+        } else if (options == "주말") {
+          // 만약
+          if (YMD_now.weekday == DateTime.saturday ||
+              YMD_now.weekday == DateTime.sunday) {
+            int weeksElapsed =
+                (DateTime.now().difference(YMD_now).inDays / 7).floor();
+            int daysToAdd = 2 * (weeksElapsed + 1); // 매주 2일씩 추가
+            monthCounts[month] = (monthCounts[month] ?? 0) + daysToAdd;
+          }
+        } else if (options == "한달") {
+          // 매월 반복 - 현재 달과 다음 달 모두 추가
+          monthCounts[month] = (monthCounts[month] ?? 0) + 1;
+          int nextMonth = (month % 12) + 1;
+          monthCounts[nextMonth] = (monthCounts[nextMonth] ?? 0) + 1;
+        } else if (options == "1년") {
+          // 매년 반복 - 현재 달에 추가
           monthCounts[month] = (monthCounts[month] ?? 0) + 1;
         } else {
           monthCounts[month] = 1;
         }
-
-        print(monthCounts);
       } else {
         print("options, monthCounts 또는 YMD_now가 null입니다.");
       }
       print(monthCounts);
     }
+
     List<FlSpot> spots = [];
     for (int i = 1; i <= 12; i++) {
       spots.add(FlSpot(i.toDouble(), (monthCounts[i] ?? 0).toDouble()));
     }
     print("======spots");
     print(spots);
-
+    List<double> scheduleCounts = spots.map((spot) => spot.y).toList();
+    double totalSchedules = scheduleCounts.reduce((a, b) => a + b);
+    print("totalSchedules"); // 37.0
+    print(totalSchedules); // 37.0
+    print("totalSchedules"); // 37.0
     return spots;
   }
 
@@ -256,6 +262,20 @@ class _calendarState extends State<calendar> {
                   // FlSpot 리스트 생성
                   List<FlSpot> spots = _generateFlSpots(documents);
                   List<FlSpot> spots2 = _generateFlSpots2(documents);
+                  List<double> scheduleCounts =
+                      spots.map((spot) => spot.y).toList();
+                  double totalSchedules =
+                      scheduleCounts.reduce((a, b) => a + b);
+                  print("totalSchedules"); // 37.0
+                  print(totalSchedules); // 37.0
+                  print("totalSchedules"); // 37.0
+                  List<double> scheduleCounts2 =
+                      spots2.map((spot) => spot.y).toList();
+                  double totalSchedules2 =
+                      scheduleCounts2.reduce((a, b) => a + b);
+                  print("totalSchedules"); // 37.0
+                  print(totalSchedules2); // 37.0
+                  print("totalSchedules"); // 37.0
                   double maxYValue = spots
                       .map((spot) => spot.y)
                       .reduce((a, b) => a > b ? a : b);
@@ -382,13 +402,18 @@ class _calendarState extends State<calendar> {
                       ElevatedButton(
                           onPressed: () {
                             print("spots");
-                            print(spots);
+                            print(totalSchedules);
                             print("spots");
                             print("spots2");
-                            print(spots2);
+                            print(totalSchedules2);
                             print("spots2");
                           },
-                          child: Text("test"))
+                          child: Text(
+                              "달성률 ${(totalSchedules2 / totalSchedules) * 100}")),
+                      Text(
+                        "달성률 ${((totalSchedules2 / totalSchedules) * 100).toStringAsFixed(1)}%",
+                        style: TextStyle(fontSize: 30),
+                      )
                     ],
                   );
                 }),
